749214764f9bb5afdab099d412610ab957ce15e6
Leandro Santos <leandro.domingosdossantos>
2025-11-11 13:59:39 +0100

    Update

---
+++ task_2_14.py
@@ -0,0 +1,11 @@
+test = int(input("teste dieses script (zahl 1-8):"))
+
+match test:
+    case 1 | 2 :
+        print("Case 1")
+    case 3 | 4 :
+        print("Case 2")
+    case 5 | 6 :
+        print("Case 3")
+    case 7 | 8 :
+        print("Case 4")
---
+++ task_3_0.py
@@ -0,0 +1,4 @@
+fish= "1,2,3"
+bomboclat= "4,5,6"
+COMBINATION = fish + "," + bomboclat
+print(COMBINATION)
